d803a1e87e87761aedaaef6b0d061014c80ddeba
monica garrigos sanchez <monicags539@gmail.com>
2022-10-22 12:03:56 +0200

    lista partidas

--- index.js
+++ index.js
@@ -1,76 +1,50 @@
 const fs = require("fs");
 const express = require('express');
-const app=express();
+const app = express();
 const modelo = require("./servidor/modelo.js");
 
 const PORT = process.env.PORT || 3000;
 
 let juego = new modelo.Juego();
 
-//HTTP GET POST PUT DELETE
-/*
-	get "/"
-	get "/obtenerPartidas"
-	post get "/agregarUsuario/:nick"
-	put "/actualizarPartida"
-	delete "/eliminarPartida"
-	"/..."
-*/
-
-/*
-app.get("/",(req,res) => {
-	res
-	  .status(200)
-	  .send("Hola")
-	  .end();
-});
-*/
 app.use(express.static(__dirname + "/"));
 
-app.get("/", function(request,response){
-	var contenido=fs.readFileSync(__dirname+"/cliente/index.html");
-	response.setHeader("Content-type","text/html");
+app.get("/", function (request, response) {
+	var contenido = fs.readFileSync(__dirname + "/cliente/index.html");
+	response.setHeader("Content-type", "text/html");
 	response.send(contenido);
 });
 
-app.get("/agregarUsuario/:nick", function(request, response){
+app.get("/agregarUsuario/:nick", function (request, response) {
 	let nick = request.params.nick;
-	let res;
-	res = juego.agregarUsuario(nick);
-	response.send(res);  				// Lo que aquí se llama res en clienteRest se llama data
+	let res = juego.agregarUsuario(nick);
+	response.send(res);
 });
 
-app.get("/crearPartida/:nick", function(request, response){
-	let  nick = request.params.nick;
+app.get("/crearPartida/:nick", function (request, response) {
+	let nick = request.params.nick;
 	let res = juego.jugadorCreaPartida(nick);
-
 	response.send(res);
-})
+});
 
-app.get("/unirseAPartida/:nick/:codigo", function(request, response){
-	let codigo = request.params.codigo;
+app.get("/unirseAPartida/:nick/:codigo", function (request, response) {
 	let nick = request.params.nick;
-
-	let res = juego.jugadorSeUneAPartida(nick,codigo);
-
+	let codigo = request.params.codigo;
+	let res = juego.jugadorSeUneAPartida(nick, codigo);
 	response.send(res);
 });
 
-app.get("/obtenerPartidas", function(request, response){
-	let res = juego.obtenerPartidas();
-
-	response.send(res);
+app.get("/obtenerPartidas", function (request, response) {
+	let lista = juego.obtenerPartidas();
+	response.send(lista);
 });
 
-app.get("/obtenerPartidasDisponibles", function(request, response){
-	let res = juego.obtenerPartidasDisponibles();
-
-	response.send(res);
+app.get("/obtenerPartidasDisponibles", function (request, response) {
+	let lista = juego.obtenerPartidasDisponibles();
+	response.send(lista);
 });
-
-//Start the server
 
 app.listen(PORT, () => {
-  console.log(`App está escuchando en el puerto ${PORT}`);
-  console.log('Ctrl+C para salir.');
+	console.log(`App está escuchando en el puerto ${PORT}`);
+	console.log('Ctrl+C para salir');
 });
