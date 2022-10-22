8625fc029292a750c05dea1984a3cdf53876a6db
monica garrigos sanchez <monicags539@gmail.com>
2022-10-22 11:57:02 +0200

    actualizacion carpeta cliente

--- cliente/clienteRest.js
+++ cliente/clienteRest.js
@@ -2,52 +2,80 @@ function ClienteRest() {
     this.nick;
 
     this.agregarUsuario = function (nick) {
-        var cli = this;
-        $.getJSON("/agregarUsuario/" + nick, function (data) {          //Función de CallBack
-            // Se ejecuta cuando ya haya contestado el servidor
+        let cli = this;
+        $.getJSON("/agregarUsuario/" + nick, function (data) {
+            //se ejecuta cuando conteste el servidor
             console.log(data);
             if (data.nick != -1) {
-                console.log("Usuario "+data.nick+" registrado");
-                cli.nick=data.nick;
-                //ws.nick = data.nick;
-                //$.cookie("nick",ws.nick);
-                iu.mostrarHome(); //iu.mostrarHome(data.nick);
+                console.log("Usuario " + data.nick + " registrado")
+                cli.nick = data.nick;
+                //ws.nick=data.nick;
+                $.cookie("nick", data.nick);
+                iu.mostrarHome();//iu.mostrarHome(data.nick)
             }
             else {
-                console.log("No se ha podido registrar el usuario");
-                //iu.mostrarModal("El nick ya está en uso");
+                console.log("No se ha podido registrar el usuario")
+                iu.mostrarModal("El nick ya está en uso");
                 iu.mostrarAgregarUsuario();
-                
             }
         });
         // Todavía no estoy seguro de que haya contestado el servidor
         // Lo que pongas aquí se ejecuta a la vez que la llamada
     }
 
-    this.crearPartida = function (nick) {
-        var cli = this;
+    this.crearPartida = function () {
+        let cli = this;
+        let nick = cli.nick;
         $.getJSON("/crearPartida/" + nick, function (data) {
             console.log(data);
             if (data.codigo != -1) {
-                console.log("El usuario "+nick+" crea la partida: "+data.codigo);
+                console.log("Usuario " + nick + " crea partida codigo: " + data.codigo)
+                iu.mostrarCodigo(data.codigo);
+                //ws.nick=data.nick;
+                //$.cookie("nick",ws.nick);
+                //iu.mostrarHome(data);
             }
-            else{
-                console.log("El usuario "+nick+" no existe");
+            else {
+                console.log("No se ha podido crear partida")
+                //iu.mostrarModal("El nick ya está en uso");
+                //iu.mostrarAgregarJugador();
             }
-        })
+        });
     }
 
-    this.unirseAPartida = function (nick, codigo) {
-        var cli = this;
-        $.getJSON("/unirseAPartida/" + nick +"/" + codigo, function (data) {
-            console.log(data);
+    this.unirseAPartida = function (codigo) {
+        let cli = this;
+        $.getJSON("/unirseAPartida/" + cli.nick + "/" + codigo, function (data) {
+            //se ejecuta cuando conteste el servidor
+            //console.log(data);
             if (data.codigo != -1) {
-                console.log("El usuario "+nick+" se ha unido a la partida: "+data.codigo);
+                console.log("Usuario " + cli.nick + " se une a partida codigo: " + data.codigo);
+                iu.mostrarCodigo(data.codigo);
+                //ws.nick=data.nick;
+                //$.cookie("nick",ws.nick);
+                //iu.mostrarHome(data);
             }
-            else{
-                console.log("El usuario "+nick+" no se ha podido unir a la partida: "+codigo);
+            else {
+                console.log("No se ha podido unir a partida")
+                //iu.mostrarModal("El nick ya está en uso");
+                //iu.mostrarAgregarJugador();
             }
-        })
+        });
+    }
+    this.obtenerListaPartidas = function () {
+        let cli = this;
+        //obtenerPartidasDisponibles
+        $.getJSON("/obtenerPartidas", function (lista) {
+            console.log(lista);
+            iu.mostrarListaDePartidas(lista);
+        });
+    }
+    this.obtenerListaPartidasDisponibles = function () {
+        let cli = this;
+        $.getJSON("/obtenerPartidasDisponibles", function (lista) {
+            console.log(lista);
+            iu.mostrarListaDePartidasDisponibles(lista);
+        });
     }
 
 }
--- cliente/controlWeb.js
+++ cliente/controlWeb.js
@@ -1,9 +1,18 @@
 function ControlWeb() {
 
+    this.comprobarCookie = function () {
+        if ($.cookie("nick")) {
+            rest.nick = $.cookie("nick");
+            this.mostrarHome();
+        }
+        else {
+            this.mostrarAgregarUsuario();
+        }
+    }
     this.mostrarAgregarUsuario = function () {   //LLAMADA AL SERVIDOR
-
-        var cadena = '<div class="row" id="mAU">';//'<form class="form-row needs-validation"  id="mAU">';
-        cadena = cadena + '<div class="row"><h2>El juego indefinido</h2></div>';
+        let cadena = '<div class="row" id="mAU">';//'<form class="form-row needs-validation"  id="mAJ">';
+        cadena = cadena + "<div class='col'>";
+        cadena = cadena + '<div class="row"><div class="col"><h2>El juego indefinido</h2></div></div>';
         cadena = cadena + '<div class="row">';
         cadena = cadena + '<div class="col">'
         cadena = cadena + '<input type="text" class="form-control mb-2 mr-sm-2" id="usr" placeholder="Introduce tu nick (max 6 letras)" required></div>';
@@ -11,7 +20,8 @@ function ControlWeb() {
         cadena = cadena + '<button id="btnAU" class="btn btn-primary mb-2 mr-sm-2">Iniciar sesión</button>';
         //cadena=cadena+'<a href="/auth/google" class="btn btn-primary mb-2 mr-sm-2">Accede con Google</a>';
         cadena = cadena + '</div>'; //' </form>';
-        cadena = cadena + '<div id="nota"></div></div></div>';
+        cadena = cadena + '<div id="nota"></div>';
+        cadena = cadena + '</div></div></div>';
 
         $("#agregarUsuario").append(cadena);
         //$("#nota").append("<div id='aviso' style='text-align:right'>Inicia sesión con Google para jugar</div>");    
@@ -24,32 +34,95 @@ function ControlWeb() {
             else {
                 var nick = $('#usr').val();
                 $("#mAU").remove();
-                $("#aviso").remove();
+                //$("#aviso").remove();
                 rest.agregarUsuario(nick);
-                //Por ejmplo ---> Aqui puedo poner un gif mientras espera a que el servidor contesta (muestra que está esperando)
+                //mostrar gif --> Por ejmplo, aqui puedo poner un gif mientras espera a que el servidor contesta (muestra que está esperando)
             }
         })
     }
 
     this.mostrarHome = function () {
-        //Cadena="Bienvenido" + irest.nick;
-        let cadena = "<div id='mH'>";
+        $('#mH').remove();
+        let cadena = "<div class='row' id='mH'>";
+        cadena = cadena + '<div class="col">';
         cadena = cadena + "<p>Bienvenido " + rest.nick + "</p>";
-        cadena = cadena + "</div>";
+        cadena = cadena + '<button id="btnSalir" class="btn btn-primary mb-2 mr-sm-2">Salir</button>';
+        cadena = cadena + "<div id='codigo'></div>"
+        cadena = cadena + "</div></div>";
         $('#agregarUsuario').append(cadena);
+        this.mostrarCrearPartida();
+        rest.obtenerListaPartidasDisponibles();
+        $("#btnSalir").on("click", function (e) {
+            $("#mCP").remove();
+            $('#mLP').remove();
+            $('#mH').remove();
+            //rest.crearPartida();
+            $.removeCookie("nick");
+            iu.comprobarCookie();
+        });
+    }
+    this.mostrarCrearPartida = function () {
+        $('#mCP').remove();
+        let cadena = '<div class="row" id="mCP">';//'<form class="form-row needs-validation"  id="mAJ">';
+        cadena = cadena + '<div class="col">';
+        cadena = cadena + '<button id="btnCP" class="btn btn-primary mb-2 mr-sm-2">Crear partida</button>';
+        cadena = cadena + '</div>';
+        cadena = cadena + '</div>';
+        $('#crearPartida').append(cadena);
+        $("#btnCP").on("click", function (e) {
+            $("#mCP").remove();
+            $('#mLP').remove();
+            rest.crearPartida();
+        });
+    }
+    this.mostrarCodigo = function (codigo) {
+        let cadena = "Código de la partida: " + codigo;
+        $('#codigo').append(cadena);
+    }
+    this.mostrarListaDePartidas = function (lista) {
+        $('#mLP').remove();
+        let cadena = "<div id='mLP'>";
+        cadena = cadena + '<ul class="list-group">';
+        for (i = 0; i < lista.length; i++) {
+            cadena = cadena + '<li class="list-group-item">' + lista[i].codigo + ' propietario: ' + lista[i].owner + '</li>';
+        }
+        cadena = cadena + "</ul>";
+        cadena = cadena + "</div>"
+        $('#listaPartidas').append(cadena);
 
     }
+    this.mostrarListaDePartidasDisponibles = function (lista) {
+        $('#mLP').remove();
+        let cadena = "<div class='row' id='mLP'>";
+        cadena = cadena + "<div class='col'>";
+        cadena = cadena + "<h2>Lista de partidas disponibles</h2>";
+        cadena = cadena + '<button id="btnAL" class="btn btn-primary mb-2 mr-sm-2">Actualizar</button>';
+        cadena = cadena + '<ul class="list-group">';
+        for (i = 0; i < lista.length; i++) {
+            cadena = cadena + '<li class="list-group-item"><a href="#" value="' + lista[i].codigo + '"> Nick propietario: ' + lista[i].owner + '</a></li>';
+        }
+        cadena = cadena + "</ul>";
+        cadena = cadena + "</div></div>"
+        $('#listaPartidas').append(cadena);
 
-
-    this.mostrarCrearPartida=function(){
-        //dibujar un boton, que al hacer click llame a crear partida de rest
+        $(".list-group a").click(function () {
+            codigo = $(this).attr("value");
+            console.log(codigo);
+            if (codigo) {
+                $('#mLP').remove();
+                $('#mCP').remove();
+                rest.unirseAPartida(codigo);
+            }
+        });
+        $("#btnAL").on("click", function (e) {
+            rest.obtenerListaPartidasDisponibles();
+        })
     }
-
-    this.mostrarListaDePartidas=function(lista){
-        //crear un control visual tipo lista para mostrar la lista de partidas y permitir unirse con un click a la partida seleccionada
-        // --> ir a boostrap (https://www.w3schools.com/bootstrap4/bootstrap_containers.asp) y ir a bs4 tables o bs4 list groups 
-
-
+    this.mostrarModal = function (msg) {
+        $('#mM').remove();
+        var cadena = "<p id='mM'>" + msg + "</p>";
+        $('#contenidoModal').append(cadena);
+        $('#miModal').modal("show");
     }
 
 }
